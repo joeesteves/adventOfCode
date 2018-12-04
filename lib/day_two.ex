3eaaa4dd247425eb3bfac715824fef723119cee6
Jose Esteves <jose@ceibo.co>
2018-12-04 13:55:57 -0300

    day 2

--- lib/day_two.ex
+++ lib/day_two.ex
@@ -1,5 +1,42 @@
 defmodule DayTwo do
 
+  def closest(data) when is_bitstring(data) do
+    data
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.to_charlist/1)
+    |> closest
+  end
+
+  def closest([head | tail]) do
+    Enum.find_value(tail, &(one_char_diff(head, &1))) || closest(tail)
+  end
+
+
+  defp one_char_diff(cl1, cl2) do
+    cl1
+    |> Enum.zip(cl2)
+    |> Enum.split_with( fn {cp1, cp2} -> cp1 == cp2 end)
+    |> case do
+      {tuples_of_codepoints, [_]} ->
+        tuples_of_codepoints
+        |> Enum.map(fn {cp, _} -> cp end)
+        |> List.to_string
+      _ -> nil
+    end
+  end
+
+
+
+  def checksum(data_string) when is_binary(data_string) do
+    { twice, thrice } = data_string
+    |> String.split("\n", trim: true)
+    |> Enum.map(&get_twice_and_thrice/1)
+    |> Enum.reduce({0, 0}, fn {twice, thrice}, {twice_acc, thrice_acc} ->
+      {twice_acc + twice, thrice_acc + thrice}
+    end)
+    twice * thrice
+  end
+
   defp get_twice_and_thrice(str_line) do
     str_line
     |> String.codepoints
@@ -13,16 +50,6 @@ defmodule DayTwo do
         _ -> { twice, thrice}
       end
     end)
-  end
-
-  def checksum(data_string) do
-    { twice, thrice } = data_string
-    |> String.split("\n", trim: true)
-    |> Enum.map(&get_twice_and_thrice/1)
-    |> Enum.reduce({0, 0}, fn {twice, thrice}, {twice_acc, thrice_acc} ->
-      {twice_acc + twice, thrice_acc + thrice}
-    end)
-    twice * thrice
   end
 
 end
--- test/day_two_test.exs
+++ test/day_two_test.exs
@@ -15,4 +15,19 @@ defmodule DayTwoTest do
 
     assert DayTwo.checksum(data) == 12
   end
+
+  @tag :two_two
+  test "get similar" do
+    data = """
+    abcde
+    fghij
+    klmno
+    pqrst
+    fguij
+    axcye
+    wvxyz
+    """
+
+    assert DayTwo.closest(data) == "fgij"
+  end
 end
