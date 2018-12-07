60376f4e6a7b0d9feab5a76cccdfd845b1615c25
Jose Esteves <jose@ceibo.co>
2018-12-07 10:00:39 -0300

    add python solutions examples

---
+++ python/day1.py
@@ -0,0 +1,25 @@
+# https://github.com/pietroppeter/AdventOfCode2018/
+# Example from @pietroppeter
+
+with open('../data/day1.txt') as f:
+    instructions = f.readlines()
+
+changes = [int(line) for line in instructions]
+
+
+def solve(changes, frequency=0, seen=None, iterations=0):
+    seen = seen or {0}
+    for change in changes:
+        frequency += change
+        if frequency in seen:
+            return frequency, iterations
+        else:
+            seen.add(frequency)
+    iterations += 1
+    return solve(changes, frequency=frequency, seen=seen, iterations=iterations)
+
+
+print('part 1')
+print(sum(changes))
+print('part 2')
+print(solve(changes))
